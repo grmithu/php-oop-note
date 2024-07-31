07fcc1749b4027d494bf2597296718f715498567
mithu <golamrabbani3010@gmail.com>
2024-07-31 22:00:51 +0600

    encapsulation

--- index.php
+++ index.php
@@ -1,28 +1,4 @@
-// Code re-use
-// Kono akti class er property/method onno akti class re-use trait dia karte parbo
-// 
+<?php 
 
+echo "  ";
 
-
-<?php
-
-
-class Post{
-    use shareable;
-}
-
-class Comment{
-    use shareable;
-}
-
-trait shareable{
-    public function share(){
-        printf(format: "Shared to social media");
-    }
-}
-
-$obj = new Post();
-echo $obj->share();
-
-
-?>
